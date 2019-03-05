0ead013aa1510919b23afdcbf356d6a0231109da
wanglei <wanglei_sy56@163.com>
2019-03-05 14:08:16 +0800

    增加model

---
+++ CreditChina/Pods/Headers/Private/WLPlatform/WLGetMainPeopleDataModel.h
@@ -0,0 +1,1 @@
+../../../../../WLLib/WLPlatform/WLPlatform/Classes/WLModels/WLGetMainPeopleDataModel.h
---
+++ CreditChina/Pods/Headers/Public/WLPlatform/WLgetCreditDataNumModel.h
@@ -0,0 +1,1 @@
+../../../../../WLLib/WLPlatform/WLPlatform/Classes/WLModels/WLgetCreditDataNumModel.h
